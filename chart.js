f91be4a47176e376ee1798b5d07109f229328916
zmey2k <zmey2k@yahoo.com>
2021-10-02 15:47:11 +0200

    dt0

--- chart.js
+++ chart.js
@@ -193,6 +193,9 @@ function myFunction() {
     const timeConv = d3.timeParse("%Y-%m-%d");
     let dataset = d3.csv("./csv_for_site/WWAllplatformsother.csv");
     dataset.then(function(data) {
+        data = data.filter(function(d) {
+            return timeConv(d.date) > timeConv(sd);
+          });
         var slices = data.columns.slice(1).map(function(id) {
             return {
                 id: id,
