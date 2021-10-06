bcf6d9b548ec6c33958a892cdb083517c7c6f729
zmey2k <zmey2k@yahoo.com>
2021-10-06 10:30:27 +0200

    dataa

--- chart.js
+++ chart.js
@@ -194,7 +194,7 @@ function myFunction() {
     let dataset = d3.csv("./csv_for_site/WWAllplatformsother.csv")
     dataset
         .then(function(d){
-            let dat0=d.filter(function(i){
+            let data=d.filter(function(i){
                 return timeConv(i.date)>timeConv('2021-08-26');
             });
             return data;
